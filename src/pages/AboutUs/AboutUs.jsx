2a707cf957ae8700b93b7e613071ded25889975d
María <xmaryjs99@gmail.com>
2025-05-19 23:59:46 +0200

    algunos cambios de estilo y taskDetails terminada

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,15 +1,37 @@
 import { Link } from "react-router-dom";
+import "./AboutUs.css";
+import userImg from "../../assets/images.png";
 
 function AboutUs() {
   return (
-    <div>
-      <h1>Maria</h1>
-      <h1>Alvaro</h1>
+    <div className="container">
+      <h1>About the App</h1>
+      <div className="app-description">
+        <p>
+          This Kanban application is a simple web app designed to visually
+          organize tasks across three columns: To Do, In Progress, and Done.
+          Users can add tasks through a form, view detailed information by
+          clicking on each task, update their status to reflect progress, and
+          delete tasks when no longer needed.
+        </p>
+      </div>
+
+      <div className="team-members">
+        <h1>About us</h1>
+        <div className="members">
+          <img src={userImg} width={"100px"} />
+          <img src={userImg} width={"100px"} />
+        </div>
+        <div className="members">
+          <h3>María</h3>
+          <h3>Alvaro</h3>
+        </div>
+      </div>
 
       <Link to="/MainBoard">
-        <button>Volver</button>
+        <button className="go-back-btn">Go back</button>
       </Link>
     </div>
   );
 }
-export default AboutUs
+export default AboutUs;
--- src/pages/TaskDetailsPage/TaskDetailPage.jsx
+++ src/pages/TaskDetailsPage/TaskDetailPage.jsx
@@ -1,20 +1,33 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./TaskDetailPage.css"
 
-function TaskDetailPage(props){
+function TaskDetailPage(props) {
+  const params = useParams();
 
-    const params = useParams()
+  const foundTask = props.taskList.find((eachTask) => {
+    return eachTask.id === params.taskId;
+  });
 
-    const foundTask = props.taskList.find((eachTask) => {
-        return eachTask.id === params.taskId
-    })
+  console.log(foundTask);
 
-    
+  return (
+    <div className="task-details">
+      <h1>Task details</h1>
 
-    console.log(foundTask)
+      <div className="task-info">
+        <h4><span>Title: </span>{foundTask.title}</h4>
+        <h4><span>Description: </span>{foundTask.description}</h4>
+        <h4><span>Assignee: </span>{foundTask.assignee}</h4>
+        <h4><span>Status: </span>{foundTask.status}</h4>
+        <h4><span>Priority: </span>{foundTask.priority}</h4>
+      </div>
 
-    return(
-        <h1>{foundTask.title}</h1>
-    )
+      <Link to="/MainBoard">
+        <button className="go-back-btn">Go back</button>
+      </Link>
+    </div>
+  );
 }
 
-export default TaskDetailPage
+export default TaskDetailPage;
